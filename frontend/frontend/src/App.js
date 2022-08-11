77f3b16f4497baaa8524a79bb0fb2bd033cd862a
sammsaski <samuel.sasaki@gmail.com>
2022-08-10 23:02:15 -0700

    List all elements in the wishlist and add elements

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -1,13 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
 import DisplayItems from './components/DisplayItems';
+import CreateItem from './components/CreateItem';
 
 function App() {
   return (
     <div>
-      <h1 className="HomePageHeader">Welcome to the home page!</h1>
-      <DisplayItems></DisplayItems>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-1"></div>
+          <div className="col-10 text-center">
+            <div className="home-text">Create a wishlist!</div>
+          </div>
+          <div className="col-1"></div>
+        </div>
+      </div>
+
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-12 text-center">
+            <div className="row d-flex justify-content-center">
+              <div className="col-6" id="extra-class">
+                <DisplayItems></DisplayItems>
+              </div>
+              
+              <div>
+                <CreateItem></CreateItem>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div id="random-div"></div>
+
     </div>
+    
+    
   );
 }
 
